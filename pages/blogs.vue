66423d72974b99dcc229bca0d6c807eda5e0c7f6
Prashant Kaushal <kaushal.prashant@gmail.com>
2024-07-29 14:04:38 +0530

    case sensitive generated contract file

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -11,7 +11,7 @@ import { createThirdwebClient,getContract,readContract,sendTransaction,resolveMe
 import { sepolia } from "thirdweb/chains";
 import { createWallet,injectedProvider } from "thirdweb/wallets";
 // import { getAllBlogs } from "~/thirdweb/11155111/0x367b2A4e271840FA2C5E8fac4C7ae7dC431Fa5a5";
-import { getAllBlogs } from "/thirdweb/11155111/0x367b2A4e271840FA2C5E8fac4C7ae7dC431Fa5a5";
+import { getAllBlogs } from "@/thirdweb/11155111/0x367b2a4e271840fa2c5e8fac4c7ae7dc431fa5a5";
 
 const runtimeConfig = useRuntimeConfig()
 
